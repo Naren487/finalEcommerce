31fe056490e6dfa25c103c3c9ddc811d892491d4
Naren487 <84893790+Naren487@users.noreply.github.com>
2023-12-05 18:36:03 +0530

    Update Cart.jsx

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -46,7 +46,7 @@ const Cart = () => {
     const headers={
       "Content-Type":"application/json"
     }
-    const response=await fetch("http://localhost:7000/api/create-checkout-session",{
+    const response=await fetch("http://184.72.88.173:7000/api/create-checkout-session",{
       method:"POST",
       headers:headers,
       body:JSON.stringify(body)
